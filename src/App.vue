d857e06125798569acb177b5d6b523adf4f65297
robertobenfini <roberto.benfini@gmail.com>
2023-06-26 16:27:14 +0200

    Milestone-1

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <script>
+//importo le componenti
 import axios from 'axios';
 
 import AppHeader from './components/AppHeader.vue';
@@ -7,6 +8,7 @@ import AppMain from './components/AppMain.vue';
 import { store } from './store.js';
 
 export default {
+
     components:{
     AppHeader,
     AppMain
@@ -16,18 +18,44 @@ export default {
       store
     }
   },
+
+  //chiamo la funzione della chiamata API
+  mounted(){
+    this.getFilms()
+  },
+
+  //funzione chiamata API
+  methods:{
+    getFilms(){
+
+      let myUrl=store.apiUrl;
+
+      if(store.filmSearch !== ''){
+        myUrl += `&query=${store.filmSearch}`
+      }
+
+      axios.get(myUrl).then((response) => {
+      store.arrayFilms = response.data.results;
+    })
+    }
+  }
 }
 </script>
 
 <template>
 
   <div>
-    <AppHeader />
+
+    <!--passo il valore dell'emit al padre-->
+    <AppHeader @film="getFilms" />
     <AppMain />
+    
   </div>
 
 </template>
 
 <style lang="scss">
+
+  //collego i file di stile
   @use './styles/generals.scss' as *;
 </style>
